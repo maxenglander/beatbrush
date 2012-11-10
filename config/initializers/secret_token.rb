13e3928323817fef5d9af3ea3858940e2896c732
Michael Hoy <michael.john.hoy@gmail.com>
2012-11-10 17:12:03 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beatbrushtest::Application.config.secret_token = 'c7a8f92a02dd4649c26bfd92bdd4dff3ceb70cbd4938116b985369d68a7b0a1b2c3180cd19bf91f5aa9808b750343a9a4571e36e241608718fbd2bd79bb8105c'
